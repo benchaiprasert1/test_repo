4c5463a9b8f5a5dacac4efab061bc31ac736688b
Benjamin Chaiprasert <bc32544@educcomp03.ccbb.utexas.edu>
2019-12-04 16:24:30 -0600

    test commit

---
+++ testfile.py
@@ -0,0 +1,7 @@
+git init #initialize git respository in your home directory
+git config --global user.username benchaiprasert1 #update this with your actual github username
+git add testfile.py #replace testfile.py with any file in your home directory you want to upload: 
+git commit -m "test commit" #this commits the file and adds a message (required)
+git remote add origin https://github.com/benchaiprasert1/test_repo.git #update with your link
+git push origin master
+
